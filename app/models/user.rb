260c60a0ff848ef990da93ee316f67faa71b9c78
takkod <99372499+takkod@users.noreply.github.com>
2023-07-12 08:47:27 +0900

    User.nicknameのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  
-  validates :nickname, length: { maximum: 20 } 
+  validates :nickname,  presence: true,  length: { maximum: 20 } 
 
 end
